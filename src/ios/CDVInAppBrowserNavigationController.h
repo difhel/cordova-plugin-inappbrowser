9907ca9ec51074924cb762bb2eaa0af38fab1d9d
Mark Fomin <mark@difhel.dev>
2026-03-24 23:29:48 +0300

    fix: restore iab navigation orientation delegate

--- src/ios/CDVInAppBrowserNavigationController.h
+++ src/ios/CDVInAppBrowserNavigationController.h
@@ -18,7 +18,10 @@
 */
 
 #import <UIKit/UINavigationController.h>
+#import <Cordova/CDVScreenOrientationDelegate.h>
 
 @interface CDVInAppBrowserNavigationController : UINavigationController
+
+@property (nonatomic, weak) id <CDVScreenOrientationDelegate> orientationDelegate;
 
 @end
